eeb4ea8ae441688ec41ea2ffe6d7b632847b9977
akavishka-ifs <akavishka@workwave.com>
2022-08-26 09:18:27 +0530

    added a service layer to abstract business logic lil bit away from Program.cs

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(options =>
  options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
  );
+builder.Services.AddScoped<DataContext, DataContext>();
 
 var app = builder.Build();
 
---
+++ Services/BudgetCategoryService.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using Welfare_App.Context;
+using Welfare_App.Entity;
+
+namespace Welfare_App.Services
+{
+    public class BudgetCategoryService
+    {
+        private DataContext _context;   
+        public BudgetCategoryService(DataContext context){
+            _context = context;
+        }
+
+    
+        public List<BudgetCategories> GetAll() {
+            return _context.BudgetCategories.ToList();  
+        }
+    }
+}
